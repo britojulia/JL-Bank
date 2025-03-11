e9d0f45b291fe2d16b359a4c5bf5c6208253842d
britojulia <juliabri1234@gmail.com>
2025-03-11 10:50:14 -0300

    iniciando projeto

---
+++ src/main/java/br/com/fiap/JL_Bank/JlBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.JL_Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/JL_Bank/JlBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.JL_Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
